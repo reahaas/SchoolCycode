fe0f6e41099d8b288f1442d23706f57e53ade582
reahaas <rea@scadafence.com>
2021-07-04 01:58:53 +0300

    add ignore files

--- Models/Student.cs
+++ Models/Student.cs
@@ -2,9 +2,18 @@ namespace SchoolCycode.Models
 {
     public class Student
     {
+
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+    
+        // public Student(int Id, string FirstName, string LastName, string Email)
+        // {
+        //     this.Id = Id;
+        //     this.FirstName = FirstName;
+        //     this.LastName = LastName;
+        //     this.Email = Email;
+        // }
     }
 }
